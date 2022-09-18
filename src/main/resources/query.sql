94c486b4e8807c661fbb1fa56b38ddde506ded2a
root <poddubniysergey1981@gmail.com>
2022-09-18 16:33:02 +0300

    Added migration with liquibase, database is changed from mysql to postgresql.

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -1,3 +1,3 @@
 select c.name, o.product_name
-from orders o join (select id, name from customers where name = :name) c on o.customer_id = c.id
+from orders o join (select id, name from customers where lower(name) = lower(:name)) c on o.customer_id = c.id
 order by o.product_name;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,13 +6,6 @@ create table customers (
                            phone_number char(11) not null
 );
 create index name_index on customers(name);
-insert into customers(name, surname, age, phone_number) values ('Alexey', 'Kust', 29, '89246545221');
-insert into customers(name, surname, age, phone_number) values ('Zevs', 'Oganesov', 26, '89235486552');
-insert into customers(name, surname, age, phone_number) values ('alexey', 'Vetrov', 44, '89222365446');
-insert into customers(name, surname, age, phone_number) values ('Marya', 'Suchova', 24, '89668783222');
-insert into customers(name, surname, age, phone_number) values ('alexey', 'Valenok', 55, '89133955885');
-insert into customers(name, surname, age, phone_number) values ('Igor', 'Scoriy', 20, '89568485544');
-insert into customers(name, surname, age, phone_number) values ('Alexey', 'Mirniy', 33, '89265244732');
 
 create table orders(
   id serial primary key,
@@ -22,8 +15,3 @@ create table orders(
   amount dec(10, 2) not null,
   foreign key (customer_id) references customers(id)
 );
-
-insert into orders(date, customer_id, product_name, amount) VALUES ('2021.12.08', 5, 'fun', 28543.45);
-insert into orders(date, customer_id, product_name, amount) VALUES ('2020.07.23', 1, 'controller', 3045.12);
-insert into orders(date, customer_id, product_name, amount) VALUES ('2022.04.12', 4, 'driver', 2747.88);
-insert into orders(date, customer_id, product_name, amount) VALUES ('2022.08.16', 2, 'fun', 16752.27);
